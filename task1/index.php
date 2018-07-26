763413150f8a57af1acc8f430fa3501690183032
ucfirst-ruslan <ucefirst.ruslan@gmail.com>
2018-07-26 20:52:41 +0300

    test

--- task1/index.php
+++ task1/index.php
@@ -1,5 +1,5 @@
 <?php
-ini_set('display_errors', 1);
+//ini_set('display_errors', 1);
 /*include ('config.php');
 include ('libs/Controller.php');
 include ('libs/View.php');
@@ -15,48 +15,56 @@ catch(Exception $e)
 	echo $e->getMessage();
 }*/
 
-$client = new SoapClient("http://www.dataaccess.com/webservicesserver/numberconversion.wso?WSDL", array('trace' => 1));
+/* $client = new SoapClient("http://www.dataaccess.com/webservicesserver/numberconversion.wso?WSDL", array('trace' => 1));
 $param = [
 	"ubiNum" => "125"
 ];
 $resNumb = $client->NumberToWords($param);
-echo "REQUEST:\n" . $client->__getLastRequest() . "\n";
-
-/*
- * $xml_post_string = '<?xml version="1.0" encoding="utf-8"?>
-<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
-  <soap:Body>
-    <GetCitiesByCountry xmlns="http://www.webserviceX.NET">
-      <CountryName>INDIA</CountryName>
-    </GetCitiesByCountry>
-  </soap:Body>
-</soap:Envelope>';
+echo "REQUEST:\n" . $client->__getLastRequest() . "\n"; */
 
 
 
-$headers = array(
-	"Content-type: text/xml;charset=\"utf-8\"",
-	"Accept: text/xml",
-	"Cache-Control: no-cache",
-	"Pragma: no-cache",
-	"SOAPAction: http://www.webservicex.com/globalweather.asmx",
-	"Content-length: ".strlen($xml_post_string),
-);
-
-$url = $soapUrl;
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+ 
+        /* $url = 'https://www.w3schools.com/xml/tempconvert.asmx/CelsiusToFahrenheit';
+		$data = array('Celsius' => '56');
 
 
-curl_setopt($ch, CURLOPT_TIMEOUT, 10);
-curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $xml_post_string); // the SOAP request
+		$ch = curl_init(); 
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_POST, 1);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+		
+		$output = curl_exec($ch); 
+		$info = curl_getinfo($ch);      
+		curl_close($ch);
 
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+		$xml = (array)simplexml_load_string($output);
 
-// converting
-$response = curl_exec($ch);
-curl_close($ch);
-echo $response;
- */
+		var_dump($xml[0]); */
+		
+		
+		$url = 'http://webservices.oorsprong.org/websamples.countryinfo/CountryInfoService.wso';
+		$data = array("sCountryISOCode"=>"UA");
+
+
+		$ch = curl_init(); 
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_POST, 1);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+		
+		$output = curl_exec($ch); 
+		$info = curl_getinfo($ch);      
+		curl_close($ch);
+
+		//$xml = (array)simplexml_load_string($output);
+
+		var_dump($output);
+
+		http://webservices.oorsprong.org/websamples.countryinfo/CountryInfoService.wso
+		
+		
+		
+		
+		
